ac0b80691d4adb5b34a125418626b6136f3b4a7d
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-10 15:21:30 -0700

    Completed initial app component construction with state and GET request

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = 'http://localhost:9000/api/todos';
 
 export default class App extends React.Component {
+  state = {
+    toDoItems: []
+  }
+
+  fetchAllToDoItems = () => {
+    axios.get(URL)
+      .then(response => {
+        this.setState({ ...this.state, toDoItems: response.data.data })
+      })
+      .catch(error => console.error(error))
+  }
+
+  componentDidMount() {
+    this.fetchAllToDoItems();
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div>
+          <h2>To Do:</h2>
+          {
+            this.state.toDoItems.map(task => {
+              return <div key={task.id}>{task.name}</div>
+            })
+          }
+        </div>
+        <form>
+          <input type='text' placeholder='Enter new task here'></input>
+          <button>Add</button>
+          <button>Clear</button>
+        </form>
+      </div>
+    )
   }
 }
